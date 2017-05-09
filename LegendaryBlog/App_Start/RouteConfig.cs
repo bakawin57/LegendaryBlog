8e15cb98412467dd25d40312f53958b0c3e71168
Bakawin57 <Bakawin57@163.com>
2017-05-09 09:33:51 +0800

    1.添加博文添加功能 2.博文浏览功能 3.博文浏览分页

--- LegendaryBlog/App_Start/RouteConfig.cs
+++ LegendaryBlog/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace LegendaryBlog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Posts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ LegendaryBlog/Controllers/PostsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using LegendaryBlog.Models;
+namespace LegendaryBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        private BlogModel model = new BlogModel();
+        private const int PostsPerPage = 2;
+        public ActionResult Index(int? id)
+        {
+            int pageNumber = id ?? 0;
+            IEnumerable<Post> posts = (from post in model.Posts
+                                      where post.DateTime < DateTime.Now
+                                      orderby post.DateTime descending
+                                      select post).Skip(pageNumber*PostsPerPage).Take(PostsPerPage+1);
+            ViewBag.IsPreviousLinkVisible = pageNumber > 0;
+            ViewBag.IsNextLinkVisible = posts.Count() > PostsPerPage;
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.IsAdmin = IsAdmin;
+
+            return View(posts.Take(PostsPerPage));
+        }
+        [ValidateInput(false)]
+        public ActionResult Update(int? id,string title,string body,DateTime dateTime,string tags)
+        {
+            Post post = GetPost(id);
+            post.Title = title;
+            post.Body = body;
+            post.DateTime = dateTime;
+            post.Tags.Clear();
+            tags = tags ?? string.Empty;
+            string[] tagNames = tags.Split(new char[] { ' '},StringSplitOptions.RemoveEmptyEntries);
+            foreach (string tagName in tagNames)
+            {
+                post.Tags.Add(GetTag(tagName));
+            }
+            if (!id.HasValue)
+            {
+                model.Posts.Add(post);
+               // model.AddToPosts(post);
+            }
+            model.SaveChanges();
+            return RedirectToAction("Details",new { id = post.Id});
+        }
+        private Tag GetTag(string tagName)
+        {
+            return model.Tags.Where(x=>x.Name==tagName).FirstOrDefault()??new Tag() { Name=tagName};
+        }
+        private Post GetPost(int? id)
+        {
+            return id.HasValue ? model.Posts.Where(x=>x.Id==id).First():new Post() { Id=-1};
+        }
+        public ActionResult Edit(int? id)
+        {
+            Post post = GetPost(id);
+            StringBuilder tagList = new StringBuilder();
+            foreach (Tag tag in post.Tags)
+            {
+                tagList.AppendFormat("{0}",tag.Name);
+
+            }
+            ViewBag.Tags = tagList.ToString();
+            return View(post);
+        }
+        public bool IsAdmin { get { return true /*return Session["IsAdmin"] != null && (bool)Session["IsAdmin"]*/; } }
+    }
+}
